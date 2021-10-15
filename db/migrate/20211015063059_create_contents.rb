c8ee0ff27da3b2697270c7001fcaf1addfa57b5f
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-10-15 15:47:28 +0900

    content

---
+++ db/migrate/20211015063059_create_contents.rb
@@ -0,0 +1,10 @@
+class CreateContents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :contents do |t|
+      t.string :title
+      t.string :text
+      t.text :image
+      t.timestamps
+    end
+  end
+end
