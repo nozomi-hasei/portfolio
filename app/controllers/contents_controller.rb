1bef50a56f293f65daa0a12555e2950d27cc3c2e
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2022-02-28 23:39:35 +0900

    update

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -16,6 +16,15 @@ class ContentsController < ApplicationController
     end
   end
 
+  def originalapp
+  end
+
+  def work
+  end
+
+  def contact
+  end
+  
   private
   def content_params
     params.permit(:company, :name, :e_mail, :message)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: "contents#index"
   resources :contents, only: [:index, :new, :create]
+    get 'contents/index', to: 'contents#originalapp'
+    get 'contents/index', to: 'contents#work'
+    get 'contents/index', to: 'contents#contact'
 end
